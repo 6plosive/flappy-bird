4cb5a08739b2f180c73993c2f7c1f9a3a4821f37
6plosive <mrsparkylol0705@icloud.com>
2024-12-27 02:30:47 +0800

    Implement dynamic pillar spawning with adjustable gap and height calculations

--- game.gd
+++ game.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
 var Pillar = preload("res://pillar.tscn")
+var gap:float = 100
 
 func spawn_pillar():
+	var screen_height = DisplayServer.window_get_size().y # Gets current window height
+		
 	var bottomPiller = Pillar.instantiate()
-	bottomPiller.initialPosition = Vector2(1200,324);
-	bottomPiller.height = 30;
-	#bottomPiller.position = Vector2(1200, 300) # Right side of screen
+	var topPiller = Pillar.instantiate()
+	
+	var gapHeight = randf_range(gap, screen_height - gap) # 100-548
+	var bottom_height = gapHeight - gap / 2
+	var top_height = screen_height - gapHeight - gap / 2
+	
+	bottomPiller.initialPosition = Vector2(1200, screen_height)
+	bottomPiller.pillarHeight = bottom_height - gap / 2
 	add_child(bottomPiller)
+	
+	topPiller.initialPosition = Vector2(1200, 0)
+	topPiller.pillarHeight = top_height - gap / 2
+	add_child(topPiller)
+
 	
 	# Add collision detection in pillar scene
 	#pillar.body_entered.connect(func(body):
@@ -18,17 +31,16 @@ func spawn_pillar():
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var screen_height = DisplayServer.window_get_size().y # Gets current window height
-
-	print_debug(screen_height)
+	#print_debug(screen_height)
 	pass # Replace with function body.
 	#spawn_pillar()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_spawn_timer_timeout() -> void:
 	#pass # Replace with function body.
+	print_debug("spawned lol")
 	spawn_pillar()
--- pillar.gd
+++ pillar.gd
@@ -1,25 +1,27 @@
 extends Area2D
 
-@export var height: int # Allows setting from inspector and code
+@export var pillarHeight: int # Allows setting from inspector and code
 @export var initialPosition: Vector2 # Allows setting from inspector and code
+
+@export var speed = 200
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#var random_height = randf_range(100, 200)
 	position = initialPosition
-	$ColorRect.size.y = height * 2
-	print_debug(height)
-	$ColorRect.position.y = -height
-	$CollisionShape2D.shape.size.y = height * 2
-	# Center the collision shape with the new height
-	#$CollisionShape2D.position.y = height 
+	$ColorRect.size.y = pillarHeight * 2
+	$ColorRect.position.y = -pillarHeight
+	# $CollisionShape2D.shape.size.y = pillarHeight * 2
+	var new_shape = RectangleShape2D.new()
+	new_shape.extents = Vector2($CollisionShape2D.shape.extents.x, pillarHeight)
+	$CollisionShape2D.shape = new_shape
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta):
-	position.x -= 5
+	position.x -= speed * delta
 	if position.x < -100:
 		queue_free()
